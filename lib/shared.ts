4d0830e19ede84015ea2d8fd0d615ba729082699
Your Name <your-email@example.com>
2026-07-16 23:27:23 +0530

    feat: configure social links and gitConfig mapping to shelakeemahesh

--- lib/shared.ts
+++ lib/shared.ts
@@ -5,7 +5,7 @@ export const docsContentRoute = '/llms.mdx/docs';
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: 'fuma-nama',
-  repo: 'fumadocs',
-  branch: 'main',
+  user: 'shelakeemahesh',
+  repo: 'Documentation',
+  branch: 'master',
 };
